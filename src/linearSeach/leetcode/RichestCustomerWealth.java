8995268b1f3a2a156f4cd2e742b7fb818c773ee2
Sohail <sohail2769@gmail.com>
2023-08-13 22:19:43 +0530

    RichestCustomerWealth

---
+++ src/linearSeach/leetcode/RichestCustomerWealth.java
@@ -0,0 +1,28 @@
+package linearSeach.leetcode;
+
+//https://leetcode.com/problems/richest-customer-wealth/
+public class RichestCustomerWealth {
+
+    public static void main(String[] args) {
+        int[][] accounts = {{1, 2, 3}, {3, 2, 1, 8}};
+        System.out.println("ANS " + maximumWealth(accounts));
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        int maxValue = 0;
+        for (int person = 0; person < accounts.length; person++) {
+            int personValue = 0;
+            for (int account = 0; account < accounts[person].length; account++) {
+                // Not 'accountValue', you missed this, get the value
+                int accountValue = accounts[person][account];
+                personValue = personValue + accountValue;
+            }
+
+            if (maxValue < personValue) {
+                maxValue = personValue;
+            }
+        }
+
+        return maxValue;
+    }
+}
