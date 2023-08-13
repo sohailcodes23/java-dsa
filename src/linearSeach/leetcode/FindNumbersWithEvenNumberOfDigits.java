4594d8c3110885c4b89200d8fa56a92cbbc1a31c
Sohail <sohail2769@gmail.com>
2023-08-13 21:57:19 +0530

    FindNumbersWithEvenNumberOfDigits

---
+++ src/linearSeach/leetcode/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,39 @@
+package linearSeach.leetcode;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+public class FindNumbersWithEvenNumberOfDigits {
+
+    public static void main(String[] args) {
+        int[] sourceArray = {12, 345, 2, 6, 7896};
+        System.out.println("EVEN ANS " + findNumbersWithEvenNumberOfDigits(sourceArray));
+        System.out.println("ODD ANS " + findNumbersWithODDNumberOfDigits(sourceArray));
+    }
+
+
+    //EVEN
+    public static int findNumbersWithEvenNumberOfDigits(int[] nums) {
+        int ans = 0;
+        for (int value : nums) {
+            int valueLength = String.valueOf(value).length();
+            if (valueLength % 2 == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    //ODD
+    public static int findNumbersWithODDNumberOfDigits(int[] nums) {
+        int ans = 0;
+        for (int value : nums) {
+            int valueLength = String.valueOf(value).length();
+            if (valueLength % 2 != 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+}
