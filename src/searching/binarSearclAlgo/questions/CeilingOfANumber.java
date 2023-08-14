c5cadf9f57b150ba7a71f4a82efc8d0a705df2a9
Sohail <sohail2769@gmail.com>
2023-08-14 22:42:59 +0530

    CeilingOfANumber

---
+++ src/searching/binarSearclAlgo/questions/CeilingOfANumber.java
@@ -0,0 +1,8 @@
+package searching.binarSearclAlgo.questions;
+
+public class CeilingOfANumber {
+    // Ceiling means the nearest greater number to the target or the target itself
+    // int[] arr={1,2,3,6,7,89}
+    // here if target is 7, than the ceiling is 7
+    // here if target is 4, than the ceiling is 6
+}
