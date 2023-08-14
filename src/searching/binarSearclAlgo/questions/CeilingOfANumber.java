2c51cb0f52e44fd2566a6b24fbf9e6a1b5edc8d6
Sohail <sohail2769@gmail.com>
2023-08-14 23:05:31 +0530

    CeilingOfANumber OWN code

--- src/searching/binarSearclAlgo/questions/CeilingOfANumber.java
+++ src/searching/binarSearclAlgo/questions/CeilingOfANumber.java
@@ -2,7 +2,40 @@ package searching.binarSearclAlgo.questions;
 
 public class CeilingOfANumber {
     // Ceiling means the nearest greater number to the target or the target itself
-    // int[] arr={1,2,3,6,7,89}
-    // here if target is 7, than the ceiling is 7
-    // here if target is 4, than the ceiling is 6
+    // int[] arr={1, 2, 3, 4, 5, 7, 9, 32, 45, 65, 87, 98};
+    // here if target is 32, than the ceiling is 32
+    // here if target is 6, than the ceiling is 7
+
+
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 2, 3, 4, 5, 7, 9, 32, 45, 65, 87, 98};
+        System.out.println("ANS " + binarySearch(sortedArray, 86));
+    }
+
+    /// OWN CODE
+    static int binarySearch(int[] array, int target) {
+
+        int start = 0;
+        int end = array.length - 1;
+        int middleIndex = 0;
+        while (start <= end) {
+
+            middleIndex = start + (end - start) / 2;
+            int middleElement = array[middleIndex];
+
+            if (middleElement > target) {
+                end = middleIndex - 1;
+            } else if (middleElement < target) {
+                start = middleIndex + 1;
+            } else {
+                return array[middleIndex];
+            }
+        }
+
+        int ceilingIndex = middleIndex + 1;
+        if (ceilingIndex >= array.length) {
+            return -1;
+        }
+        return array[ceilingIndex];
+    }
 }
