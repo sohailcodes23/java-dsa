801797cf4d28efcfe1b938e0fd5eb5ac7b44f3ea
Sohail <sohail2769@gmail.com>
2023-08-24 08:54:55 +0530

    searchInRotatedUniqueSortedArray

--- src/searching/binarSearclAlgo/leetcode/SearchInRotatedArray.java
+++ src/searching/binarSearclAlgo/leetcode/SearchInRotatedArray.java
@@ -1,28 +1,25 @@
 package searching.binarSearclAlgo.leetcode;
 
-//https://leetcode.com/problems/peak-index-in-a-mountain-array/
-//https://leetcode.com/problems/find-peak-element/
-//Bitonic Array = PeekIndexInMountain
-//        PeekIndexInMountain= Largest number in the array // provided there wn't be dublicate values
+//https://leetcode.com/problems/search-in-rotated-sorted-array/
 public class SearchInRotatedArray {
 
     public static void main(String[] args) {
         int[] sortedArray = {3, 4, 5, 6, 7, 0, 1, 2};
-        System.out.println("ANS " + searchInRotatedUniqueArray(sortedArray, 4));
+        System.out.println("ANS " + searchInRotatedUniqueSortedArray(sortedArray, 4));
     }
 
 
     // Own Code
     // Just follow sorted property of Binary Search
-    static int searchInRotatedUniqueArray(int[] arr, int target) {
+    static int searchInRotatedUniqueSortedArray(int[] nums, int target) {
 
         int start = 0;
-        int end = arr.length - 1;
+        int end = nums.length - 1;
         while (start <= end) {
             int middleIndex = start + (end - start) / 2;
-            int middleElement = arr[middleIndex];
-            int startElement = arr[start];
-            int endElement = arr[end];
+            int middleElement = nums[middleIndex];
+            int startElement = nums[start];
+            int endElement = nums[end];
             if (middleElement == target) {
                 return middleIndex;
             } else if (startElement <= middleElement) { // if this is true than target is on left side of middle
