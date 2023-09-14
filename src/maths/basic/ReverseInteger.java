b82231b9e6994c8ef1ddf12ee01481bc8c7883e6
Sohail <sohail2769@gmail.com>
2023-09-14 09:20:12 +0530

    Basic Maths ReverseInteger fixed

--- src/maths/basic/ReverseInteger.java
+++ src/maths/basic/ReverseInteger.java
@@ -4,13 +4,13 @@ package maths.basic;
 public class ReverseInteger {
     //9646324351
     public static void main(String[] args) {
-        System.out.println(reverse(-123));
+        System.out.println(reverse1(1534236469));
 
     }
 
     // Not working for 1534236469, working for rest of the test cases
     public static int reverse1(int x) {
-        int finalNumber = 0;
+        long finalNumber = 0;
         // Replacing  while (x > 0) { with  while (x != 0) {, so that you can directly use the x variable rather than converting it to a positive number
         while (x != 0) {
             int lastDigit = x % 10;
@@ -20,14 +20,10 @@ public class ReverseInteger {
         if (finalNumber < Integer.MIN_VALUE || finalNumber > Integer.MAX_VALUE) {
             return 0;
         }
-
-//        if (x < 0) {
-//            return -1 * finalNumber;
-//        }
-
-        return finalNumber;
+        return (int) finalNumber;
     }
 
+    //Works in all scenario, but we can't use long according to question
     public static int reverse(int x) {
         long finalNum = 0;
         while (x != 0) {
