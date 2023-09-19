e8f7572d3356faec39e8782be6269da70bf81495
Sohail <sohail2769@gmail.com>
2023-09-19 22:11:49 +0530

    IMP : Hashing : HashingUsingMap

--- src/hashing/basic/BasicHashing.java
+++ src/hashing/basic/BasicHashing.java
@@ -4,8 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+// READ : https://takeuforward.org/hashing/hashing-maps-time-complexity-collisions-division-rule-of-hashing-strivers-a2z-dsa-course/
+
 // Note IMP
-//if you add more than allowed  maxSize OR maxValue  it will throw Segmentation error // NOTE, I checked Integer hashing with 10 power of 8 It works for me in side main
+//if you add more than allowed  maxValue  it will throw Segmentation error // NOTE, I checked Integer hashing with 10 power of 8 It works for me in side main
 // For Integer : Max Size in side main function can be 10 power of 6
 // For Integer : Max Size globally can be 10 power of 7
 
@@ -15,6 +17,18 @@ import java.util.Scanner;
 
 
 // No limitation for hash size for characters, bcz max it can go if you add lower and upper case is 256
+
+// Best and average case TS when HashMap is used=> 0(1) i.e. constant
+// Worst case TS when HashMap is used=> 0(N) i.e. N is number of elements in array, This occurs very rarely.
+
+//Our first priority will be always to use unordered_map(HashMap) and then map. If unordered_map gives a time limit exceeded error(TLE), we will then use the map.
+//The time complexity in the worst case is O(N) because of the internal collision.
+
+//Hashing methods :
+//        1. Division method : In short the values are % by 10 and saved on that key, like 28%10 = 8(28 will be stored in a chain at key 8), 12%10= 2 (12 will be stored in a chain at key 2)
+// Collisions: if multiple(Or all)  values are stored at a single key than collisions happens and then TS is worst case i.e. O(N)
+//        2. Folding method
+//        3. Mid-Square method
 public class BasicHashing {
 
     public static void main(String[] args) {
@@ -33,10 +47,10 @@ public class BasicHashing {
         int array[] = new int[n];
         // give the values of array
         //  pre-compute
-        //  maxSize OR maxValue +1 //
+        //  maxValue +1 //
         // if the input is 1,2,3,6,1,2, than while iterating after 3 it won't store for '4' value as 0, it will be assumed 0 by default, it will direct store '6' in the stash.
         // This is difference between using array and HashMap
-        HashMap<Integer, Integer> hash = new HashMap<>(); // Here whatever problem states the maximum size of array are allowed add one, like if max size of array can be 12 than write 13, if 20 than 21,  maxSize OR maxValue +1
+        HashMap<Integer, Integer> hash = new HashMap<>(); // Here whatever problem states the maximum value in array are allowed add one, like if max size of array can be 12 than write 13, if 20 than 21,  maxValue +1
         for (int i = 0; i < n; i++) {
             array[i] = scanner.nextInt();
 
@@ -52,7 +66,7 @@ public class BasicHashing {
 
         // Remove this
         //  pre-compute
-        //  maxSize OR maxValue +1 //
+        //  maxValue +1 //
         // if the input is 1,2,3,6,1,2, than while iterating after 3 it won't store for '4' value as 0, it will be assumed 0 by default, it will direct store '6' in the stash.
         // This is difference between using array and HashMap
 //        for (int i = 0; i < n; i++) {
@@ -65,6 +79,13 @@ public class BasicHashing {
 //            freq++;
 //            hash.put(key, freq);
 //        }
+
+        // Iterate over the map:
+        /*
+        for (Map.Entry<Integer, Integer> it : mp.entrySet()) {
+            System.out.println(it.getKey() + "->" + it.getValue());
+        }
+        */
 
         // take the queries for which you numbers you want to print the occurrence of numbers
 
@@ -92,10 +113,10 @@ public class BasicHashing {
             array[i] = scanner.nextInt();
         }
         //  pre-compute
-        //  maxSize OR maxValue +1 //
+        //  maxValue +1 //
         // if the input is 1,2,3,6,1,2, than while iterating after 3 it won't store for '4' value as 0, it will be assumed 0 by default, it will direct store '6' in the stash. 
         // This is difference between using array and HashMap
-        HashMap<Integer, Integer> hash = new HashMap<>(); // Here whatever problem states the maximum size of array are allowed add one, like if max size of array can be 12 than write 13, if 20 than 21,  maxSize OR maxValue +1
+        HashMap<Integer, Integer> hash = new HashMap<>(); // Here whatever problem states the maximum value in array are allowed add one, like if max size of array can be 12 than write 13, if 20 than 21,  maxValue +1
         for (int i = 0; i < n; i++) {
             // Updating hashmap
             int key = array[i];
@@ -107,8 +128,14 @@ public class BasicHashing {
             hash.put(key, freq);
         }
 
-        // take the queries for which you numbers you want to print the occurrence of numbers
+        // Iterate over the map:
+        /*
+        for (Map.Entry<Integer, Integer> it : mp.entrySet()) {
+            System.out.println(it.getKey() + "->" + it.getValue());
+        }
+        */
 
+        // take the queries for which you numbers you want to print the occurrence of numbers
         int q = scanner.nextInt(); // this is the number of query values which we want to check in the array
         while (q-- != 0) { // we are decreasing till it's not = 0, EX if 5 than decreasing till 0
             int number = scanner.nextInt(); // taking the queries
@@ -159,7 +186,7 @@ public class BasicHashing {
         String value = scanner.next();    // We are assuming all the characters are lower case
 
         //pre-compute
-//        Since the assumption is for all lower case, There are total 25 lower case alphabets. So  maxSize OR maxValue +1
+//        Since the assumption is for all lower case, There are total 25 lower case alphabets. So  maxValue +1
         int[] hash = new int[26];
         for (int i = 0; i < value.length(); i++) {
             hash[value.charAt(i) - 'a']++; // EX => a = 97 and f=102, if i=f, than f-1 i.e. 102-93=5. So at 5th index it will store the value and increment it if again f is found.
@@ -188,8 +215,8 @@ public class BasicHashing {
             array[i] = scanner.nextInt();
         }
         //pre-compute
-        //  maxSize OR maxValue +1 //
-        int[] hash = new int[13]; // Here whatever problem states the maximum size of array are allowed add one, like if max size of array can be 12 than write 13, if 20 than 21,  maxSize OR maxValue +1
+        //  maxValue +1 //
+        int[] hash = new int[13]; // Here whatever problem states the maximum value in array are allowed add one, like if max size of array can be 12 than write 13, if 20 than 21,  maxValue +1
         for (int i = 0; i < n; i++) {
             // assigning the value of hash
             // so if array[1] is 3, than in hash at the 3rd index it will add one, same goes for other values as well.
