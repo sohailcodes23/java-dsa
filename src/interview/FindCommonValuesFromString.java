00fca9dcd1606a6b4a6317a9b7c8e2a6e2b96cda
Sohail <sohail2769@gmail.com>
2023-10-30 23:01:45 +0530

    Array : GetSingleElementFromArray

---
+++ src/interview/FindCommonValuesFromString.java
@@ -0,0 +1,40 @@
+package interview;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindCommonValuesFromString {
+
+    public static void main(String[] args) {
+        String str1 = "ABCDCD"; // ABCD
+        String str2 = "CDEFD";
+
+        // Expected O/P=> CDD
+        System.out.println("ANS " + findCommonValue(str1, str2));
+    }
+
+    public static String findCommonValue(String str1, String str2) {
+
+
+        // Create a HashMap to store characters and their counts from the first string
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+        for (char c : str1.toCharArray()) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        }
+
+        // StringBuilder to store common characters
+        StringBuilder commonChars = new StringBuilder();
+
+        // Check characters of the second string against the HashMap
+        for (char c : str2.toCharArray()) {
+            if (charCountMap.containsKey(c) && charCountMap.get(c) > 0) {
+                commonChars.append(c);
+                charCountMap.put(c, charCountMap.get(c) - 1);
+            }
+        }
+
+        return commonChars.toString();
+
+    }
+
+}
