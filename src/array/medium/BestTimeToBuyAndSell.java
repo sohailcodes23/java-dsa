cc01146e75e333eca6f3e302189a08e5ec11c5d5
Sohail <sohail2769@gmail.com>
2023-11-12 17:18:34 +0530

    Array : BestTimeToBuyAndSell

---
+++ src/array/medium/BestTimeToBuyAndSell.java
@@ -0,0 +1,27 @@
+package array.medium;
+
+// Negative values are not taken as minimum
+// checked only on the right side of the minium number
+public class BestTimeToBuyAndSell {
+
+    public static void main(String[] args) {
+
+        int array[] = {8, 1, 5, 4, 3, 6,7};
+
+        System.out.println("ANS " + findMaxProfit(array));
+    }
+
+    static int findMaxProfit(int prices[]) {
+
+        int mini = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int cost = prices[i] - mini;
+            maxProfit = Math.max(cost, maxProfit);
+            mini = Math.min(mini, prices[i]);
+
+        }
+
+        return maxProfit;
+    }
+}
