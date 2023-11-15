920aba664e42b135862dc1938cb796c50de1e6f5
Sohail <sohail2769@gmail.com>
2023-11-15 14:29:42 +0530

    Array : RearrangeElementBySign

--- src/array/medium/RearrangeElementBySign.java
+++ src/array/medium/RearrangeElementBySign.java
@@ -12,39 +12,70 @@ import java.util.List;
 public class RearrangeElementBySign {
 
     public static void main(String[] args) {
-        int array[] = {-2, -3, 4, 5,-1};
-//        int array[] = {3, 1, -2, -5, 2, -4};
+//        int array[] = {-2, -3, 7, 5, 1, 4, 8, -3};
+        int array[] = {3, 1, -2, -5, 2, -4};
 //        int array[] = {-3, 1, 3, 2, -5, -4};
 
 //        rearrangeElementBySign_Optimal(array);
-        System.out.println("ANS " + Arrays.toString(rearrangeElementBySign_Optimal(array)));
-        System.out.println("ANS " + Arrays.toString(rearrangeElementBySign_Optimalv2(array)));
+//        System.out.println("ANS " + Arrays.toString(rearrangeElementBySign_Optimal(array)));
+//        System.out.println("ANS " + Arrays.toString(rearrangeElementBySign_Optimalv2(array)));
 
-        rearrangeElementBySign_Brute(array);
-        System.out.println("ANS " + Arrays.toString(array));
+        System.out.println("ANS " + Arrays.toString(rearrangeElementBySign_Optimalv2(array)));
     }
 
-    static int[] rearrangeElementBySign_Optimalv2(int a[]) {
-        // Final TC => O(N)
-        // SC => O(N)
-        int positiveIndex = 0;
-        int negativeIndex = 1;
-        int ans[] = new int[a.length];
-        for (int i = 0; i < a.length; i++) {
 
+    static int[] rearrangeElementBySign_Optimalv2(int a[]) {
+        // Final TC => O(N) + O(N) = O(2N)
+        // SC +> O(N)
+
+        List<Integer> positives = new ArrayList<>();
+        List<Integer> negatives = new ArrayList<>();
+
+// TC => O(N)
+        for (int i = 0; i < a.length; i++) {
             if (a[i] > 0) {
-                ans[positiveIndex] = a[i];
-                positiveIndex = (positiveIndex + 2) >= a.length ? positiveIndex + 1 : positiveIndex + 2; // this is added so if the +ves and -ves are not equal it will assign the values at the end f the array
+                positives.add(a[i]);
             } else {
-                ans[negativeIndex] = a[i];
-                negativeIndex = (negativeIndex + 2) >= a.length ? negativeIndex + 1 : negativeIndex + 2;
+                negatives.add(a[i]);
             }
         }
 
-        return ans;
+        if (positives.size() > negatives.size()) {
+
+            for (int i = 0; i < negatives.size(); i++) { // negatives.size() will be the lower size of array, so that all the negative values are added in the array
+
+                a[2 * i] = positives.get(i);
+                a[2 * i + 1] = negatives.get(i);
+            }
+
+            int startIndex = negatives.size() * 2;
+
+            for (int i = negatives.size(); i < positives.size(); i++) { // to add rest pending elements
+
+                a[startIndex] = positives.get(i);
+                startIndex++;
+            }
+        } else {
+
+
+            for (int i = 0; i < positives.size(); i++) { // positives.size() will be the lower size of array, so that all the positive values are added in the array
+
+                a[2 * i] = positives.get(i);
+                a[2 * i + 1] = negatives.get(i);
+            }
+
+            int startIndex = positives.size() * 2;
+
+            for (int i = positives.size(); i < negatives.size(); i++) { // to add rest pending elements
+
+                a[startIndex] = negatives.get(i);
+                startIndex++;
+            }
+
+        }
+
+        return a;
     }
-
-
     static int[] rearrangeElementBySign_Optimal(int nums[]) {
         // Final TC => O(N)
         // SC => O(N)
@@ -66,7 +97,7 @@ public class RearrangeElementBySign {
     }
 
     static void rearrangeElementBySign_Brute(int array[]) {
-// FInal TC => O(N) + O(N/2)
+// Final TC => O(N) + O(N/2)
         // SC +> O(N)
 
         List<Integer> positives = new ArrayList<>();
@@ -88,4 +119,31 @@ public class RearrangeElementBySign {
             array[2 * i + 1] = negatives.get(i);
         }
     }
+
+
+
+    // Code studio took the ans but the code doesn't return the required answer
+    static int[] rearrangeElementBySign_(int a[]) {
+        // Final TC => O(N)
+        // SC => O(N)
+        int positiveIndex = 0;
+        int negativeIndex = 1;
+        int ans[] = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+
+            if (a[i] > 0) {
+                ans[positiveIndex] = a[i];
+                positiveIndex = (positiveIndex + 2) >= a.length ? positiveIndex + 1 : positiveIndex + 2; // this is added so if the +ves and -ves are not equal it will assign the values at the end f the array
+            } else {
+                ans[negativeIndex] = a[i];
+                int min = a.length - negativeIndex;
+                System.out.println(negativeIndex + " M " + min);
+                negativeIndex = (negativeIndex + 2) >= a.length ? negativeIndex + 1 : negativeIndex + 2;
+                System.out.println("N " + negativeIndex);
+
+            }
+        }
+        return ans;
+    }
+
 }
