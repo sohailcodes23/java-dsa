df8bb1fd49c7dec43b810be589b0f1ecc3e20283
Sohail <sohail2769@gmail.com>
2023-11-16 08:31:49 +0530

    Array : LongestSuccessiveElement

---
+++ src/array/medium/LongestSuccessiveElement.java
@@ -0,0 +1,41 @@
+package array.medium;
+
+import java.util.Arrays;
+
+// Difference between elements should be 1, array can be reordered/sorted
+public class LongestSuccessiveElement {
+
+    public static void main(String[] args) {
+
+//        int array[] = {5,8, 3, 2, 4, 1};
+        int array[] = {1, 2, 2, 1};
+
+        int i = longestSuccessiveElements(array);
+
+        System.out.println("ANS " + i);
+    }
+
+    public static int longestSuccessiveElements(int[] a) {
+
+        Arrays.sort(a);
+
+        int n = a.length;
+        int lastSmallest = Integer.MIN_VALUE; // to compare it should be the smallest
+        int count = 0; // count when a smallest number is found and than increment
+        int longestSequence = 1; // 1 assuming the longest we can get is 1
+        for (int i = 0;i<n;i++) {
+
+            if (a[i] - 1 == lastSmallest) { // a[i] - 1 to check if it's consecutive( the difference is 1 or not)
+                count += 1;
+                lastSmallest = a[i];
+            } else if (lastSmallest != a[i]) {
+                count = 1;
+                lastSmallest = a[i];
+            }
+
+            longestSequence = Math.max(longestSequence, count);
+        }
+
+        return longestSequence;
+    }
+}
