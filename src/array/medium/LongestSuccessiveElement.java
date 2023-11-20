2235b77261bd76cb47f0795a82f08d9ef186546d
Sohail <sohail2769@gmail.com>
2023-11-20 10:49:09 +0530

    Array : LongestSuccessiveElement

--- src/array/medium/LongestSuccessiveElement.java
+++ src/array/medium/LongestSuccessiveElement.java
@@ -15,23 +15,26 @@ public class LongestSuccessiveElement {
         System.out.println("ANS " + i);
     }
 
-    public static int longestSuccessiveElements(int[] a) {
+    public static int longestSuccessiveElements(int[] nums) {
 
+        if (nums.length == 0) {
+            return 0;
+        }
 //        TC=> O(N)
-        Arrays.sort(a); // Sort SC=> O(N log N)
+        Arrays.sort(nums); // Sort SC=> O(N log N)
 
-        int n = a.length;
+        int n = nums.length;
         int lastSmallest = Integer.MIN_VALUE; // to compare it should be the smallest
-        int count = 0; // count when a smallest number is found and than increment
+        int count = 0; // count when nums smallest number is found and than increment
         int longestSequence = 1; // 1 assuming the longest we can get is 1
-        for (int i = 0;i<n;i++) {
+        for (int i = 0; i < n; i++) {
 
-            if (a[i] - 1 == lastSmallest) { // a[i] - 1 to check if it's consecutive( the difference is 1 or not)
+            if (nums[i] - 1 == lastSmallest) { // nums[i] - 1 to check if it's consecutive( the difference is 1 or not)
                 count += 1;
-                lastSmallest = a[i];
-            } else if (lastSmallest != a[i]) {
+                lastSmallest = nums[i];
+            } else if (lastSmallest != nums[i]) {
                 count = 1;
-                lastSmallest = a[i];
+                lastSmallest = nums[i];
             }
 
             longestSequence = Math.max(longestSequence, count);
