eb3ec6e15660911a78829aabc150916e6ab18895
Sohail <sohail@pop-os.localdomain>
2023-11-23 10:00:43 +0530

    Practice : Array : PositiveNegativePair

---
+++ src/practice/array/PositiveNegativePair.java
@@ -0,0 +1,70 @@
+package practice.array;
+
+import java.util.*;
+
+public class PositiveNegativePair {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+//        integers.addAll(Arrays.asList(13, 14, 3, 8, 2));
+        integers.addAll(Arrays.asList(1, -3, -2, 3, 6, -1)); // -1 1  -3 3
+        System.out.println("ANS " + pairs_Brute(integers, integers.size()));
+        System.out.println("ANS " + pairs_Better(integers, integers.size()));
+    }
+
+    public static ArrayList<ArrayList<Integer>> pairs_Better(ArrayList<Integer> arr, int n) {
+//        TC=>O(2N)
+//        SC=> O(N)
+        ArrayList<ArrayList<Integer>> arrayLists = new ArrayList<>();
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < arr.size(); i++) { // TC => O(N)
+
+            hashMap.put(arr.get(i), i);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) { // TC => O(N)
+            int entryKey = entry.getKey();
+            if (entryKey < 0) {
+                ArrayList<Integer> newPairs = new ArrayList<>();
+                int positiveValue = entryKey * -1;
+
+                if (hashMap.containsKey(positiveValue) && hashMap.get(positiveValue) != -1) {
+
+                    newPairs.add(entry.getKey());
+                    newPairs.add(positiveValue);
+                    hashMap.put(positiveValue, -1);
+                }
+
+                if (!newPairs.isEmpty()) {
+                    arrayLists.add(newPairs);
+                }
+            }
+        }
+
+        return arrayLists;
+    }
+
+    public static ArrayList<ArrayList<Integer>> pairs_Brute(ArrayList<Integer> arr, int n) {
+
+        Collections.sort(arr); // TC=> O(N logN)
+        ArrayList<ArrayList<Integer>> arrayLists = new ArrayList<>();
+        // TC=> O(N ^ 2)
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) < 0) {
+                int positiveValue = arr.get(i) * -1;
+                ArrayList<Integer> newPairs = new ArrayList<>();
+                for (int j = i + 1; j < arr.size(); j++) {
+                    if (positiveValue == arr.get(j)) {
+                        newPairs.add(arr.get(i));
+                        newPairs.add(arr.get(j));
+                    }
+                }
+                if (!newPairs.isEmpty()) {
+                    arrayLists.add(newPairs);
+                }
+            }
+        }
+
+        return arrayLists;
+    }
+}
