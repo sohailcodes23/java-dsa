99c83409df2c12a72eb91c5c9ccbf1c4144c7027
Sohail <sohail@pop-os.localdomain>
2023-11-28 13:01:10 +0530

    Practice : Array : NonDecreasingArray

--- src/practice/array/NonDecreasingArray.java
+++ src/practice/array/NonDecreasingArray.java
@@ -12,7 +12,8 @@ public class NonDecreasingArray {
 //        int array[]={8,4,6}; // update 0th index with 2
 //        int array[] = {-2, 7, -1, 0, 1, 2}; // update 1st index with -2
 //        int array[] = {8, 4, 2}; // false
-        int array[] = {1, 3, 2}; // false
+        int array[] = {3,4,2,3}; // false
+//        int array[] = {1, 3, 2}; // false
 //        int array[] = {4, 2, 3}; // true
 //        int array[] = {5, 7, 1, 8}; // true
 
@@ -40,11 +41,86 @@ public class NonDecreasingArray {
 
 //        System.out.println("ANS " + isPossible_Brute(array, array.length));
 //        System.out.println("ANS " + isPossible(array, array.length));
-        System.out.println("ANS " + isPossiblev2(array, array.length));
+//        System.out.println("ANS " + checkPossibility_Better(array));
+        System.out.println("ANS " + checkPossibility_Optimal(array));
+//        System.out.println("ANS " + checkPossibility_Optimal(array));
     }
 
-    public static boolean isPossiblev2(int[] arr, int n) {
+    public static boolean checkPossibility_Optimal(int[] nums) {
 
+//        TC=> O(N)
+//        SC=> O(1)
+        int n = nums.length;
+        boolean changed = false;
+
+        for (int i = 0; i < n-1; i++) {
+            if (nums[i] > nums[i + 1]) {
+
+                if (changed) {// this is wihtin the condition, so if it's true that means it has been changed once
+                    return false;
+                }
+
+//                EX 1: 4 2 3
+//              if i>i+1
+//                if i at 0th than
+//                        i=i+1
+//                if i !=0 than (i+1)[3] >= (i-1)[2]
+//                        i=i+1
+//                else i+1= i
+
+//                EX 1: 8 4 2
+//              if i>i+1
+//                if i at 0th than
+//                        i=i+1 [4 4 2]
+//                if i !=0 than (i+1)[2] >= (i-1)[8]
+//                        i=i+1
+//                else i+1= i
+
+
+                if (i == 0 || nums[i + 1] >= nums[i - 1]) {
+                    nums[i] = nums[i + 1];
+                } else {
+                    nums[i + 1] = nums[i];
+                }
+
+                changed = true;
+            }
+        }
+
+        return true;
+    }
+
+    // Another way
+    public static boolean checkPossibility_Optimalv2(int[] nums) {
+
+        int count = 0;
+        //We can edit atmost 1 value, so break the loop if the count exceeds 1.
+        for (int i = 1; i < nums.length; i++) {
+            //Checking prev value with curr value
+            if (nums[i - 1] > nums[i]) {
+                System.out.println("BB " + Arrays.toString(nums));
+                count++;
+                //checking the second prev value from curr value.
+                if (i - 2 < 0 || nums[i - 2] <= nums[i])
+                    nums[i - 1] = nums[i];    //If prev value is > current val, change the prev value.
+                else
+                    nums[i] = nums[i - 1];    //If last 2 values both are > current, change the current value.
+            }
+            if (count > 1) {
+                break;
+            }
+        }
+        return count <= 1;
+    }
+
+
+
+
+
+    public static boolean checkPossibility_Brute(int[] arr, int n) {
+
+//        TC=> O(2N)
+//        SC=> O(1)
         boolean changed = false;
         for (int i = 0; i < n - 1; i++) {
 
