da5c40929d45b41a4a59b38dc6ef7f2bee2818a6
Sohail <sohail@pop-os.localdomain>
2023-11-29 19:16:33 +0530

    Practice : Array : MinimumParentheses

---
+++ src/practice/string/MinimumParentheses.java
@@ -0,0 +1,34 @@
+package practice.string;
+
+public class MinimumParentheses {
+
+    public static void main(String[] args) {
+//        String input = "()))(("; // 0
+        String input = ")((()"; // 0
+
+        System.out.println("ANS " + minAddToMakeValid(input));
+    }
+    public static int minAddToMakeValid(String s) {
+
+        int open = 0;
+        int close = 0;
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {// check if any bracket is opened
+
+                open++;
+            } else {
+                // any time there is close parenthesis it will be here
+
+                if (open == 0) { // till now any bracket hasn't not been opened like ')'
+                    close++; // just to keep the close brcket data
+                } else { // this basically means that the current bracket is a close one, but it is after the open one like '()'
+                    open--;
+                }
+            }
+
+        }
+        return (open + close);
+
+    }
+}
