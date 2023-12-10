08e9c78f03fb1551b0b7b2aebc13a59734715403
sohailcodes23 <sohail2769@gmail.com>
2023-12-10 22:37:10 +0530

    Practice : Map/Set : CountSubArraysHavingSumDivisibleByK

--- src/practice/map_sets/CountSubArraysHavingSumDivisibleByK.java
+++ src/practice/map_sets/CountSubArraysHavingSumDivisibleByK.java
@@ -1,35 +1,79 @@
 package practice.map_sets;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+//https://www.youtube.com/watch?v=QM0klnvTQzk
 public class CountSubArraysHavingSumDivisibleByK {
 
     public static void main(String[] args) {
 
-        int[] array = {4, 5, 0, -2, -3, 1};
-//        int[] array = {5, 0, 2, 3, 1};
+//        int[] array = {4, 5, 0, -2, -3, 1}; // k=5  OP:7
+//        int[] array = {5, 0, 2, 3, 1}; // k=5 OP:6
+//        int[] array = {2, 3, 5, 4, 5, 3, 4}; // k=7 OP:4
+        int[] array = {-1, 2, 9}; // k=2 OP:2
 
-        System.out.println("ANS " + subarraysDivByK(array, 5));
+        System.out.println("ANS " + subarraysDivByK(array, 2));
     }
 
+    // leetcode
     public static int subarraysDivByK(int[] nums, int k) {
 
         int n = nums.length;
         HashMap<Integer, Integer> hashMap = new HashMap<>();
-        int ans = 0;
+
+        hashMap.put(0, 1);//{remainder, occurence} // required to calculate
+        int maxSubArrayCount = 0;
+        int sum = 0;
         for (int i = 0; i < n; i++) {
 
-            ans = nums[i] + ans;
-            int remainder = ans % k;
+            sum = nums[i] + sum;
 
-            hashMap.put(remainder, hashMap.getOrDefault(remainder, 0) + 1);
-            System.out.println(i + " " + remainder + " " + hashMap.get(remainder));
+            int remainder = sum % k;
+
+            if (remainder < 0) { // Special case, if remainder is -ve than we need to update add it with k, for explanation check above youtube link, basically if we add +k and -k in the equation than we will get a new remainder
+                remainder = k + remainder;
+            }
+            if (hashMap.containsKey(remainder)) {
+                maxSubArrayCount = hashMap.get(remainder) + maxSubArrayCount;
+            }
+            hashMap.put(remainder, hashMap.getOrDefault(remainder, 0) + 1); // we always want to increment the remainder
+
+
         }
 
-        ans = hashMap.get(0) + 1;
+
+        return maxSubArrayCount;
+    }
+
+    //codestudio
+    public static int subArrayCount(ArrayList<Integer> nums, int k) {
+
+        int n = nums.size();
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        hashMap.put(0, 1);//{remainder, occurence} // required to calculate
+        int maxSubArrayCount = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+
+            sum = nums.get(i) + sum;
+
+            int remainder = sum % k;
+
+            if (remainder < 0) { // Special case, if remainder is -ve than we need to update add it with k, for explanation check above youtube link, basically if we add +k and -k in the equation than we will get a new remainder
+                remainder = k + remainder;
+            }
+            if (hashMap.containsKey(remainder)) {
+                maxSubArrayCount = hashMap.get(remainder) + maxSubArrayCount;
+            }
+            hashMap.put(remainder, hashMap.getOrDefault(remainder, 0) + 1); // we always want to increment the remainder
 
 
-        return ans ;
+        }
+
+
+        return maxSubArrayCount;
     }
 
     public static int subarraysDivByK_Brute(int[] nums, int k) {
