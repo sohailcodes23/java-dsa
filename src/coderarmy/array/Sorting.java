8bb2b3395482336322fecd8987a0c1ad80909a6d
sohailcodes23 <sohail2769@gmail.com>
2024-02-10 20:35:09 +0530

    Day 4  : Sorting

--- src/coderarmy/array/Sorting.java
+++ src/coderarmy/array/Sorting.java
@@ -8,14 +8,25 @@ public class Sorting {
 
     public static void main(String[] args) {
 
-        int array[] = {4, 6, 2, 58, 3, 65};
-        selectionSort(array);
+        int array[] = {4, 6, 2, 58, 69, 3, 65};
+//        selectionSortv2(array);
+        selectionSortv2(array);
 
+//        int largest = 0;
+//        for (int j = 0; j < array.length; j++) {
+//            if (array[j] > array[largest]) {
+//                largest = j;
+//            }
+//        }
+//
+//        System.out.println(largest + " ANS " + Arrays.toString(array));
         System.out.println(" ANS " + Arrays.toString(array));
     }
 
+    // swapping smallest element at the start
     public static void selectionSort(int[] array) {
-
+        // TC=> O(N^2)
+        // SC=> O(1): constant
         int n = array.length;
 
         for (int i = 0; i < n; i++) {
@@ -31,4 +42,30 @@ public class Sorting {
         }
     }
 
+    //swapping largest element at the end
+
+
+    public static void selectionSortv2(int[] array) {
+        // TC=> O(N^2)
+        // SC=> O(1): constant
+        int n = array.length;
+        for (int i = n - 1; i > 0; i--) {
+            int largestIndex = i;
+
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[largestIndex]) { // Change the condition to sort in descending order
+                    largestIndex = j;
+                }
+            }
+
+            swapBasedOnIndex(largestIndex, i, array);
+        }
+    }
+
+
+    public static void swapBasedOnIndex(int index1, int index2, int[] array) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
 }
