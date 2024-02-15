628cabb0778e2725200826fb88de4a3e875bf106
sohailcodes23 <sohail2769@gmail.com>
2024-02-15 22:50:21 +0530

    Day 7: Binary Search

--- src/coderarmy/binarysearch/BinarySearch.java
+++ src/coderarmy/binarysearch/BinarySearch.java
@@ -5,8 +5,9 @@ public class BinarySearch {
 
     public static void main(String[] args) {
 
-        int array[] = {30, 28, 20, 16, 4};
-//        System.out.println("ANS "+searchv2(array,4));
+//        int array[] = {30, 28, 20, 16, 4};
+        int array[] = {1, 3, 5, 6};
+        System.out.println("ANS " + searchInsert(array, 5));
     }
 
 
@@ -58,5 +59,28 @@ public class BinarySearch {
 
         return -1;
 
+    }
+
+    // if found return index else index of where should be the target
+    public static int searchInsert(int[] nums, int target) {
+
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        int middle = 0;
+        while (start <= end) {
+            // to handle integer overflow
+            middle = start + (end - start) / 2;// you can also write, middle = end + (start- end)/2 : but in some case this will throw error, so use optimal one
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                start = middle + 1;
+            } else { // if middle > target
+                end = middle - 1;
+            }
+
+        }
+
+        return start;// bcz if the target element is not present than start is the index where it should have be
     }
 }
