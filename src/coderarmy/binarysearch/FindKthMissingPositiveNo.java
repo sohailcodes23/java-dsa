e982c2a9b03c5eb64ebaccdd8e3754755750a153
sohailcodes23 <sohail2769@gmail.com>
2024-02-20 00:20:39 +0530

    Day 10: Binary Search : FindKthMissingPositiveNo

--- src/coderarmy/binarysearch/FindKthMissingPositiveNo.java
+++ src/coderarmy/binarysearch/FindKthMissingPositiveNo.java
@@ -4,7 +4,7 @@ public class FindKthMissingPositiveNo {
 
     public static void main(String[] args) {
 
-        int array[] = {2, 3, 4, 7, 11};
+        int array[] = {2, 3, 4, 7, 11, 12};
 
         System.out.println("ANS " + findKthPositive(array, 5));
     }
@@ -14,21 +14,25 @@ public class FindKthMissingPositiveNo {
         // Actual: 1 2 3 4 5  6
         // Diff  : 1 1 1 3 6  6 == > (arr[mid] - mid - 1)
 
+//        TC=> O(log N)
+//        SC=> O(1)
+
         int n = arr.length;
-        int ans = -1;
+        int ans = arr.length;// assigning array size bcz if all the elements are at their 1th index position, than we need to handle tha
         int start = 0;
         int end = n - 1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if ((arr[mid] - mid - 1) <= k) {//(arr[mid] - mid - 1) = to the diff of element and the actual elements that should have been there, check above
-                start = mid + 1;
-            } else {// move left
+            if ((arr[mid] - mid - 1) >= k) {//(arr[mid] - mid - 1) = to the diff of element and the actual elements that should have been there, check above
                 ans = mid;// we are adding this with end bcz, it will move to left, bcz right is already >= k
                 end = mid - 1;
+            } else {// move left
+                start = mid + 1;
             }
         }
 
+        // Here ans is the index where that actual positive no. was supposed to be there
         return ans + k;
     }
 }
