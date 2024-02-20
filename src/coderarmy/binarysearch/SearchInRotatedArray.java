aaf16f10044bb221c0b58b70c3e999e8b57f3e5d
sohailcodes23 <sohail2769@gmail.com>
2024-02-20 18:41:52 +0530

    Day 10: Binary Search : SearchInRotatedArray v2

--- src/coderarmy/binarysearch/SearchInRotatedArray.java
+++ src/coderarmy/binarysearch/SearchInRotatedArray.java
@@ -4,8 +4,9 @@ public class SearchInRotatedArray {
 
     public static void main(String[] args) {
 
-        int array[] = {4, 5, 6, 7, 0, 1, 2};
-        System.out.println("ANS " + search(array, 1));
+//        int array[] = {4, 5, 6, 7, 0, 1, 2};
+        int array[] = {1, 0, 1, 1, 1};
+        System.out.println("ANS " + search(array, 0));
     }
 
     public static int search(int[] nums, int target) {
@@ -42,4 +43,41 @@ public class SearchInRotatedArray {
         return ans;
 
     }
+
+    public boolean searchV2(int[] nums, int target) {
+//        TC=> O(log N)
+//        SC=> O(1)
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[start] == nums[mid]) {
+
+                start++;
+                // continue;
+            } else if (nums[mid] >= nums[start]) {// left side is sorted, so the element is on the right side
+
+                if (nums[start] <= target && nums[mid] >= target) {// bcz if mid >= target that means the target is at left side
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+
+                if (nums[end] >= target && nums[mid] <= target) {// if mid<=target that means target is at right
+                    start = mid + 1;
+
+                } else {
+                    end = mid - 1;
+
+                }
+
+            }
+        }
+        return false;
+    }
+
 }
