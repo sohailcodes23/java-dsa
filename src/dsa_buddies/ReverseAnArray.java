a439d5fbab4c940fa3d1693f03bb243174627184
sohailcodes23 <sohail2769@gmail.com>
2024-11-21 08:04:22 +0530

    Day 32: ReverseAnArray

---
+++ src/dsa_buddies/ReverseAnArray.java
@@ -0,0 +1,45 @@
+package dsa_buddies;
+
+public class ReverseAnArray {
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+    }
+
+    public static void reverseString(char[] s) {
+//        TC=O(N)
+//        SC=O(1)
+        int n = s.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            swap(i, n - i - 1, s);
+        }
+
+        System.out.println(s);
+    }
+
+    private static void swap(int start, int end, char[] s) {
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+    }
+
+
+    public void reverseString_Brute(char[] s) {
+        int n = s.length;
+        char[] newArr = new char[n];
+        int j = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            newArr[j] = s[i];
+            j++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            s[i] = newArr[i];
+        }
+
+    }
+
+
+}
