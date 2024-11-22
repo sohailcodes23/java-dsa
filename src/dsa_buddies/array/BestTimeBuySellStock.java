76a62f0e937e82ddb7f5db7e55a2489ee1b9aedd
sohailcodes23 <sohail2769@gmail.com>
2024-11-23 00:43:18 +0530

    Day 32: BestTimeBuySellStock

---
+++ src/dsa_buddies/array/BestTimeBuySellStock.java
@@ -0,0 +1,44 @@
+package dsa_buddies.array;
+
+public class BestTimeBuySellStock {
+
+    public static void main(String[] args) {
+//        int[] a = {7, 1, 5, 3, 6, 4};
+        int[] a = {1, 5, 3, 6, 4, 8};
+
+        System.out.println("M " + maxProfit(a));
+    }
+
+    public static int maxProfit(int[] arr) {
+//       TC==> O(N)
+        int n = arr.length;
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (min > arr[i]) {// updating the min value
+                min = arr[i];
+            } else {// max = current - min
+                max = Math.max(max, arr[i] - min);
+            }
+        }
+
+        return max;
+
+    }
+
+    public static int maxProfit_Brute(int[] arr) {
+//       TC==> O(N^2)
+        int n = arr.length;
+
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n && arr[j] > arr[i]; j++) {
+                max = Math.max(max, arr[j] - arr[i]);
+            }
+        }
+
+        return max;
+    }
+}
--- src/dsa_buddies/array/ReverseAnArray.java
+++ src/dsa_buddies/array/ReverseAnArray.java
@@ -1,10 +1,13 @@
 package dsa_buddies.array;
+
 //https://leetcode.com/problems/reverse-string/solutions/6053158/0-ms-runtime-beats-100-user-confirm-step-by-steps-solution-beginner-friendly/
 public class ReverseAnArray {
 
     public static void main(String[] args) {
         char[] s = {'h', 'e', 'l', 'l', 'o'};
+        int[] arr = {1, 2, 3, 4};
         reverseString(s);
+        reverseArray(arr);
     }
 
     public static void reverseString(char[] s) {
@@ -17,6 +20,24 @@ public class ReverseAnArray {
         }
 
         System.out.println(s);
+    }
+
+    public static void reverseArray(int[] s) {
+//        TC=O(N)
+//        SC=O(1)
+        int n = s.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            swap(i, n - i - 1, s);
+        }
+
+        System.out.println(s);
+    }
+
+    private static void swap(int start, int end, int[] s) {
+        int temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
     }
 
     private static void swap(int start, int end, char[] s) {
