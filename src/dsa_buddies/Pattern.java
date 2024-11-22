5bfac4c8118911af5e146f34645f2b701bf0a5c1
sohailcodes23 <sohail2769@gmail.com>
2024-11-23 00:11:15 +0530

    Day 32: Nothing

--- src/dsa_buddies/Pattern.java
+++ src/dsa_buddies/Pattern.java
@@ -7,10 +7,9 @@ public class Pattern {
 
         int n = 5;
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<=i;j++){
-                System.out.print((char)('A'+j));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) ('A' + j));
             }
             System.out.println();
         }
