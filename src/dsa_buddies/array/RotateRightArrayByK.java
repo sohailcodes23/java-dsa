2c8376088da3ac55e4d3d5f6ae827673923bf703
sohailcodes23 <sohail2769@gmail.com>
2024-11-23 14:17:13 +0530

    Day 33: RotateLeftArrayByK and RotateRightArrayByK

---
+++ src/dsa_buddies/array/RotateRightArrayByK.java
@@ -0,0 +1,64 @@
+package dsa_buddies.array;
+
+import java.util.Arrays;
+
+public class RotateRightArrayByK {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2}; // 2 1
+//        int arr[] = {1, 2, 3, 4, 5, 6, 7}; // 4 5 1 2 3
+//        int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+
+        rotateArr(arr, 3);
+        System.out.println("TT " + Arrays.toString(arr));
+    }
+
+    static void rotateArr(int arr[], int d) {
+        // TC=> d + N + N ==> O(2N) ==> O(N)
+        int n = arr.length;
+        if (n > 1) {
+            d = d % n;
+            reverseArray(0, n - d - 1, arr); // Reverse from 0 to n-d // TC=> O(d)
+            reverseArray(n - d, n - 1, arr);// Reverse from n-d to end // TC=> O(N)
+            reverseArray(0, n - 1, arr);// Reverse from 0 to end  // TC=> O(N)
+        }
+    }
+
+    static void reverseArray(int i, int j, int[] arr) {
+        while (i <= j) {// while loop so we can swap all the elements
+            swapBasedOnIndex(i, j, arr);
+            i++;
+            j--;
+        }
+    }
+
+    public static void swapBasedOnIndex(int j, int i, int[] array) {
+        int tempValue = array[j]; // IMP store the value not the index
+        array[j] = array[i];
+        array[i] = tempValue;
+    }
+
+    static void rotateArr_Brute(int arr[], int d) {
+        // add your code here
+        int n = arr.length;
+        int[] temp = new int[n];
+        int j = 0;
+        // Move all the elemenst to left
+        for (int i = d; i < n; i++) {
+            temp[j] = arr[i];
+            j++;
+        }
+
+        //Mpove d starting elements to end
+        int a = 0;
+        for (int i = j; i < n; i++) {
+            temp[i] = arr[a];
+            a++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+        System.out.println("TT " + Arrays.toString(arr));
+    }
+}
