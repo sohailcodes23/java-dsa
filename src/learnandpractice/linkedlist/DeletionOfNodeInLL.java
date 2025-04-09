e122b5798d44f514cee5dec1b0bd4bff4c8af1ca
sohailcodes23 <sohail2769@gmail.com>
2025-04-10 02:37:14 +0530

    Insertion and Deletion of node in LL

--- src/learnandpractice/linkedlist/DeletionOfNodeInLL.java
+++ src/learnandpractice/linkedlist/DeletionOfNodeInLL.java
@@ -5,7 +5,7 @@ public class DeletionOfNodeInLL {
     public static void main(String[] args) {
         int a[] = {1, 2, 3, 4, 5, 6};
 //        int a[] = {4, 2, 1, 54, 6};
-        Node head = ConvertArrayToLL.convertArrayToLL(a);
+        Node head = convertArrayToLL(a);
         if (head == null) {// if there is no head, like when array is empty
             return;
         }
@@ -92,4 +92,20 @@ public class DeletionOfNodeInLL {
             traverseNode = traverseNode.next;
         }
     }
+
+
+    static Node convertArrayToLL(int a[]) {
+        Node head = new Node(a[0]);
+        Node mover = head;// Created another node for logic bcz we always try to avoid updating head
+        // If head is updated we will forget the starting point of the LL
+
+        for (int i = 1; i < a.length; i++) {
+            Node temp = new Node(a[i]);
+            mover.next = temp;// linking mover to the next element, but after this we need to move mover to next node to link it to next node(elements)
+            mover = temp;//move mover to next node(temp) place, in next loop temp will be at next index and we can keep moving mover after linking it to temp
+        }
+
+        return head;
+    }
+
 }
--- src/learnandpractice/linkedlist/InsertionOfNodeInLL.java
+++ src/learnandpractice/linkedlist/InsertionOfNodeInLL.java
@@ -1,14 +1,12 @@
 package learnandpractice.linkedlist;
 
-import util.CommonUtility;
-
-public class InserionOfNodeInLL {
+public class InsertionOfNodeInLL {
 
     public static void main(String[] args) {
         int a[] = {1, 2, 3};
 //        int a[] = {1, 2, 3, 4, 5, 6};
 //        int a[] = {4, 2, 1, 54, 6};
-        Node head = ConvertArrayToLL.convertArrayToLL(a);
+        Node head = convertArrayToLL(a);
         if (head == null) {// if there is no head, like when array is empty
             return;
         }
@@ -102,4 +100,20 @@ public class InserionOfNodeInLL {
             traverseNode = traverseNode.next;
         }
     }
+
+
+    static Node convertArrayToLL(int a[]) {
+        Node head = new Node(a[0]);
+        Node mover = head;// Created another node for logic bcz we always try to avoid updating head
+        // If head is updated we will forget the starting point of the LL
+
+        for (int i = 1; i < a.length; i++) {
+            Node temp = new Node(a[i]);
+            mover.next = temp;// linking mover to the next element, but after this we need to move mover to next node to link it to next node(elements)
+            mover = temp;//move mover to next node(temp) place, in next loop temp will be at next index and we can keep moving mover after linking it to temp
+        }
+
+        return head;
+    }
+
 }
